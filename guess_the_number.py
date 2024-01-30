71c9b0b24321e1b606a92167411c908fe8bb325a
Anatolii <noob1917.ru@gmail.com>
2024-01-30 14:31:00 +0300

    first my filter

--- guess_the_number.py
+++ guess_the_number.py
@@ -120,6 +120,7 @@ async def process_negative_answer(message: Message):
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def process_numbers_answer(message: Message):
     if users[message.from_user.id]['in_game']:
+        print(message.from_user.id)
         if int(message.text) == users[message.from_user.id]['secret_number']:
             users[message.from_user.id]['in_game'] = False
             users[message.from_user.id]['total_games'] += 1
