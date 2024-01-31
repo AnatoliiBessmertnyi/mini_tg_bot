4ace02f704cdafcb625cbc8a84f557391c7ea9e3
Anatolii <noob1917.ru@gmail.com>
2024-01-31 20:28:15 +0300

    делаем кофниг

---
+++ config_data/config.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class DatabaseConfig:
+    database: str         # Название базы данных
+    db_host: str          # URL-адрес базы данных
+    db_user: str          # Username пользователя базы данных
+    db_password: str      # Пароль к базе данных
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DatabaseConfig
+
+
+
+def load_config(path: str | None = None) -> Config:
+
+    env: Env = Env()
+    env.read_env(path)
+
+    # Добавляем проверку для ADMIN_IDS
+    admin_ids = env('ADMIN_IDS')
+    if ',' in admin_ids:
+        admin_ids = list(map(int, admin_ids.split(',')))
+    else:
+        admin_ids = [int(admin_ids)]
+
+    return Config(
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN'),
+            admin_ids=list(map(int, env.list('ADMIN_IDS')))
+        ),
+        db=DatabaseConfig(
+            database=env('DATABASE'),
+            db_host=env('DB_HOST'),
+            db_user=env('DB_USER'),
+            db_password=env('DB_PASSWORD')
+        )
+    )
