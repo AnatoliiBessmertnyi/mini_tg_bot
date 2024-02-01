8fd848f6a0620e2a57d84560e9adcca4528279f5
Anatolii <noob1917.ru@gmail.com>
2024-02-01 14:57:54 +0300

    KeyboardBuilder

--- learn/bot_keyboard.py
+++ learn/bot_keyboard.py
@@ -1,8 +1,8 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
-                           ReplyKeyboardRemove)
+from aiogram.types import KeyboardButton, Message
 from environs import Env
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 env = Env()
@@ -14,16 +14,19 @@ BOT_TOKEN = env('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-# Создаем объекты кнопок
-button_1 = KeyboardButton(text='Собак 🦮')
-button_2 = KeyboardButton(text='Огурцов 🥒')
+# Инициализируем билдер
+kb_builder = ReplyKeyboardBuilder()
 
-# Создаем объект клавиатуры, добавляя в него кнопки
-keyboard = ReplyKeyboardMarkup(
-    keyboard=[[button_1, button_2]],
-    resize_keyboard=True,
-    one_time_keyboard=True
-)
+# Создаем первый список с кнопками
+buttons_1: list[KeyboardButton] = [
+    KeyboardButton(text=f'Кнопка {i + 1}') for i in range(10)
+]
+
+# Распаковываем список с кнопками методом add
+kb_builder.add(*buttons_1)
+
+# Явно сообщаем билдеру сколько хотим видеть кнопок в 1-м и 2-м рядах
+kb_builder.adjust(2, 1, repeat=True)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -31,26 +34,8 @@ keyboard = ReplyKeyboardMarkup(
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='Чего кошки боятся больше?',
-        reply_markup=keyboard
-    )
-
-
-# Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
-@dp.message(F.text == 'Собак 🦮')
-async def process_dog_answer(message: Message):
-    await message.answer(
-        text='Да, несомненно, кошки боятся собак. '
-             'Но вы видели как они боятся огурцов?'
-    )
-
-
-# Этот хэндлер будет срабатывать на ответ "Огурцов 🥒" и удалять клавиатуру
-@dp.message(F.text == 'Огурцов 🥒')
-async def process_cucumber_answer(message: Message):
-    await message.answer(
-        text='Да, иногда кажется, что огурцов '
-             'кошки боятся больше'
+        text='Вот такая получается клавиатура',
+        reply_markup=kb_builder.as_markup(resize_keyboard=True)
     )
 
 
