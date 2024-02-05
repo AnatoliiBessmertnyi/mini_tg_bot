9f97142606a151dff109e7fc82efd2cd15395627
Anatolii <noob1917.ru@gmail.com>
2024-02-05 17:40:11 +0300

    алерты

--- learn/inline_keyboard.py
+++ learn/inline_keyboard.py
@@ -53,7 +53,10 @@ async def process_button_1_press(callback: CallbackQuery):
             text='Была нажата БОЛЬШАЯ КНОПКА 1',
             reply_markup=callback.message.reply_markup
         )
-    await callback.answer()
+    await callback.answer(
+        text='Ура! Нажата кнопка 1',
+        show_alert=True
+    )
 
 
 # Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
@@ -65,7 +68,7 @@ async def process_button_2_press(callback: CallbackQuery):
             text='Была нажата БОЛЬШАЯ КНОПКА 2',
             reply_markup=callback.message.reply_markup
         )
-    await callback.answer()
+    await callback.answer(text='Ура! Нажата кнопка 2')
 
 
 if __name__ == '__main__':
