7dbd7f7cb6b2ff4e776e37e19cb27bb14b508705
Anatolii <noob1917.ru@gmail.com>
2024-02-08 13:38:39 +0300

    морской бой

--- learn/callback_data.py
+++ learn/callback_data.py
@@ -56,14 +56,19 @@ async def process_start_command(message: Message):
     await message.answer(
         text='Вот такая клавиатура',
         reply_markup=markup
-)
+    )
 
 
-# Этот хэндлер будет срабатывать на нажатие любой
-# инлайн кнопки и распечатывать апдейт в терминал
-@dp.callback_query()
-async def process_any_inline_button_press(callback: CallbackQuery):
-    print(callback.model_dump_json(indent=4, exclude_none=True))
+# Этот хэндлер будет срабатывать на нажатие любой инлайн кнопки
+# и отправлять в чат форматированный ответ с данными из callback_data
+@dp.callback_query(GoodsCallbackFactory.filter())
+async def process_category_press(callback: CallbackQuery,
+                                 callback_data: GoodsCallbackFactory):
+    await callback.message.answer(
+        text=f'Категория товаров: {callback_data.category_id}\n'\
+             f'Подкатегория товаров: {callback_data.subcategory_id}\n'\
+             f'Товар: {callback_data.item_id}'
+    )
     await callback.answer()
 
 
