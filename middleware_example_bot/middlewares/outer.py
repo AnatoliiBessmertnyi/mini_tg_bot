69db55838f3fdba0989396f444df55e973d7fa42
Anatolii <noob1917.ru@gmail.com>
2024-02-19 13:41:12 +0300

    теневой бан

--- middleware_example_bot/middlewares/outer.py
+++ middleware_example_bot/middlewares/outer.py
@@ -71,3 +71,34 @@ class ThirdOuterMiddleware(BaseMiddleware):
         logger.debug('Выходим из миддлвари  %s', __class__.__name__)
 
         return result
+
+
+# # Пример теневого бана
+# from typing import Any, Awaitable, Callable, Dict
+
+# from aiogram import BaseMiddleware
+# from aiogram.types import TelegramObject, User
+
+# CACHE = {
+#     'banned': [254443334, 214454432, 112221212],
+# }
+
+
+# class ShadowBanMiddleware(BaseMiddleware):
+
+#     async def __call__(
+#         self,
+#         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+#         event: TelegramObject,
+#         data: Dict[str, Any]
+#     ) -> Any:
+        
+#         user: User = data.get('event_from_user')
+#         if user is not None:
+#             if user.id in CACHE.get('banned'):
+#                 return
+
+#         return await handler(event, data)
+
+
+
